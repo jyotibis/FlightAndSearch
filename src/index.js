674a41fecdd28482621d19f7059aa91493c6ec6d
jyotibis <jyotibisht7683@gmail.com>
2023-03-14 11:09:20 +0530

    setup basic dotenv

---
+++ src/index.js
@@ -0,0 +1,24 @@
+const express = require("express");
+// const bodyParser = require("body-parser");
+const { PORT } = require('./config/serverConfig');
+
+// const db = require('./models/index');
+// const {Airplane} = require('./models/index');
+
+const setupAndStartServer = async () => {
+
+    // create the express object
+    const app = express();
+    const PORT = 3000;
+
+    // app.use(bodyParser.json());
+    // app.use(bodyParser.urlencoded({ extended: true }));
+
+    app.listen(PORT, () => {
+
+        console.log(`Server started at ${PORT}`);
+
+    })
+}
+
+setupAndStartServer();
