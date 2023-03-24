b8a08f23fbc204d92c447d369449a8a087bd338a
jyotibis <jyotibisht7683@gmail.com>
2023-03-24 15:39:38 +0530

    added seeder file

--- src/index.js
+++ src/index.js
@@ -2,30 +2,27 @@ const express = require("express");
 const bodyParser = require("body-parser");
 
 const { PORT } = require('./config/serverConfig');
-
 const ApiRoutes = require('./routes/index');
 
-
-// const db = require('./models/index');
+const db = require('./models/index');
 // const {Airplane} = require('./models/index');
 
-const setupAndStartServer = async () => {
+const setupAndStartServer = async() => {
 
     // create the express object
     const app = express();
-    const PORT = 3000;
-
+    // const PORT = 3000;
     app.use(bodyParser.json());
     app.use(bodyParser.urlencoded({ extended: true }));
 
     app.use('/api', ApiRoutes);
 
-    app.listen(PORT, () => {
-
+    app.listen(PORT, async() => {
         console.log(`Server started at ${PORT}`);
-
-
-    })
+        if (process.env.SYNC_DB) {
+            db.sequelize.sync({ alter: true });
+        }
+    });
 }
 
 setupAndStartServer();
